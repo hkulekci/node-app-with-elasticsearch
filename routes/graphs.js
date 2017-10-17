e5ed663c4c1b49385121e133c8f1e16f2967adc5
Haydar KULEKCI <haydarkulekci@gmail.com>
2017-10-17 22:14:12 +0300

    added graph examples

--- routes/graphs.js
+++ routes/graphs.js
@@ -1,8 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var productGraphService = require('../services/productGraphService');
+var waterfall = require('async/waterfall');
+
+var dynamicColors = function() {
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
+
 
 router.get('/', function(req, res, next) {
-  res.render('graphs');
+
+  waterfall(
+    [
+      function(waterfallCallback) {
+        productGraphService.getProductCountByDate({}, function(err, result) {
+          if(err) { waterfallCallback(true, {}); }
+          var colors = [];
+          for (var i in result['vals']) {
+            colors.push(dynamicColors());
+          }
+          waterfallCallback(false, {"chart1": {'labels':'"' + result['keys'].join('","') + '"', 'datasets': [ {'data': result['vals'].join(','), 'label': 'Num of Product', 'colors': '"'+colors.join('","')+'"'} ]}});
+          return;
+        });
+      },
+      function(data, waterfallCallback) {
+        productGraphService.getProductQuantities({}, function(err, result) {
+          if(err) { waterfallCallback(true, {}); }
+          var colors = [];
+          for (var i in result['vals']) {
+            colors.push(dynamicColors());
+          }
+          data['chart2'] = {'labels':'"' + result['keys'].join('","') + '"', 'datasets': [ {'data': result['vals'].join(','), 'label': 'Total Product Quantity', 'colors': '"'+colors.join('","')+'"'}, {'data': result['counts'].join(','), 'label': 'Num of Product'} ]};
+          waterfallCallback(false, data);
+          return;
+        });  
+      },
+      function(data, waterfallCallback) {
+        productGraphService.getCategoriyQuantitySum({}, function(err, result) {
+          if(err) { waterfallCallback(true, {}); }
+          var colors1 = [];
+          var colors2 = [];
+          for (var i in result['vals']) {
+            colors1.push(dynamicColors());
+          }
+
+          for (var i in result['counts']) {
+            colors2.push(dynamicColors());
+          }
+          data['chart3'] = {'labels':'"' + result['keys'].join('","') + '"', 'datasets': [ {'data': result['vals'].join(','), 'label': 'Total Quantity of Category', 'colors': '"'+colors1.join('","')+'"'  }, {'data': result['counts'].join(','), 'label': 'Num of Product by Category', 'colors': '"'+colors2.join('","')+'"'} ]};
+          waterfallCallback(false, data);
+          return;
+        });  
+      }
+    ],
+    function(err, data) {
+      if(err) { res.send(500,"Server Error"); return; }
+      console.log(data);
+      res.render('graphs', data);
+    }
+  );
   return;
 });
 
---
+++ services/productGraphService.js
@@ -0,0 +1,128 @@
+var db = require('./../libraries/elasticsearch');
+
+exports.getProductCountByDate = function(params, callback) {
+  body = {
+    "size": 0, 
+    "aggs": {
+      "product_counts": {
+        "date_histogram": {
+          "field": "created_at",
+          "interval": "week"
+        }
+      }
+    }
+  };
+
+  db.search({
+    index: 'products',
+    type: 'product',
+    body: body
+  }).then(function (resp) {
+    var aggs = resp.aggregations.product_counts.buckets;
+    var result = {
+      "keys": [],
+      "vals": []
+    };
+    for (i in aggs) {
+      result['keys'].push(aggs[i].key_as_string);
+      result['vals'].push(aggs[i].doc_count);
+    }
+    callback(false, result);
+    return;
+  }, function (err) {
+    callback(true);
+    console.trace(err.message);
+    return;
+  });
+};
+
+exports.getCategoriyQuantitySum = function(params, callback) {
+  body = {
+    "size": 0, 
+    "aggs": {
+      "product_categories": {
+        "terms": {
+          "field": "categories.id",
+          "size": 10
+        },
+        "aggs": {
+          "sum": {
+            "sum": {
+              "field": "quantity"
+            }
+          }
+        }
+      }
+    }
+  };
+
+  db.search({
+    index: 'products',
+    type: 'product',
+    body: body
+  }).then(function (resp) {
+    var aggs = resp.aggregations.product_categories.buckets;
+    var result = {
+      "keys": [],
+      "vals": [],
+      "counts": []
+    };
+    for (i in aggs) {
+      result['keys'].push(aggs[i].key);
+      result['counts'].push(aggs[i].doc_count);
+      result['vals'].push(aggs[i].sum.value);
+    }
+    callback(false, result);
+    return;
+  }, function (err) {
+    callback(true);
+    console.trace(err.message);
+    return;
+  });
+};
+
+
+exports.getProductQuantities = function(params, callback) {
+  body = {
+    "size": 0, 
+    "aggs": {
+      "product_counts": {
+        "date_histogram": {
+          "field": "created_at",
+          "interval": "week"
+        },
+        "aggs": {
+          "product_quantities": {
+            "sum": {
+              "field": "quantity"
+            }
+          }
+        }
+      }
+    }
+  };
+
+  db.search({
+    index: 'products',
+    type: 'product',
+    body: body
+  }).then(function (resp) {
+    var aggs = resp.aggregations.product_counts.buckets;
+    var result = {
+      "keys": [],
+      "vals": [],
+      "counts": []
+    };
+    for (i in aggs) {
+      result['keys'].push(aggs[i].key_as_string);
+      result['counts'].push(aggs[i].doc_count);
+      result['vals'].push(aggs[i].product_quantities.value);
+    }
+    callback(false, result);
+    return;
+  }, function (err) {
+    callback(true);
+    console.trace(err.message);
+    return;
+  });
+};
